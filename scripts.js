8e77d9990188895b8af56866584b7b8a87cfd830
Juan Spada <spadajuan94@gmail.com>
2021-05-17 02:23:46 -0300

    ultimos cambios

--- scripts.js
+++ scripts.js
@@ -6,13 +6,14 @@ const editPhoneValue = () => {
     editedValue = phoneInput.value.trim();
     editedValue = editedValue.split(' ').join('')
     editedValue = editedValue.split('+').join('')
+    editedValue = editedValue.split('-').join('')
     phoneInput.value = editedValue;
 }
 const sendMessage = () => {
     editPhoneValue();
     if(phoneInput.value.length > 8 && phoneInput.value.length < 18){
         let url = "https://api.whatsapp.com/send?text=" + messageInput.value + "&phone=" + "+" + phoneInput;
-        // window.open(url);
+        window.open(url);
         validatorMessage.style.display = "none";
     }else{
         validatorMessage.style.display = "block";
